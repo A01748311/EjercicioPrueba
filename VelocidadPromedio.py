ba4ace6b91efae55fcc970fd0b9cb2ce5586cd6c
A01748311 <46821591+A01748311@users.noreply.github.com>
2019-01-22 11:01:25 -0600

    Delete VelocidadPromedio.py

--- VelocidadPromedio.py
+++
@@ -1,6 +0,0 @@
-#Autor: Santiago España Vázquez A01748311
-#Programa velocidad promedio de viaje
-
-time=int(input("¿Cuantas horas duro su viaje? "))
-distance=int(input("¿Cual fue la distancia en Km que recorrio? "))
-print("La velocidad promedio con la que realizo su viaje es de: ", distance/time, " Km/H")
